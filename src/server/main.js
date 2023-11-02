aa0d245c6bec93becbc85633c9c99ba75dc989dc
ChrisNieves <nievescs20@gmail.com>
2023-11-02 18:00:08 -0400

    added route for users

--- src/server/main.js
+++ src/server/main.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const ViteExpress = require("vite-express");
+const prisma = require("./client");
 
 const app = express();
 
 app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.status(200).send({ users });
 });
 
 ViteExpress.listen(app, 3000, () =>

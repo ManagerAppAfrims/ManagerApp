c23ade12d405c425503967fc11ac92a8bee2d840
ChrisNieves <nievescs20@gmail.com>
2023-11-13 11:07:12 -0500

    added css for handling mobile navbar

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,7 +1,4 @@
-import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { getUserThunk } from "./store/reducers/player";
 import RouterComponent from "./components/RouterComponent";
 import Navbar from "./components/Navbar";
 

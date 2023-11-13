7edc7fd85f58b85c7ec916e5bd6cf39c3a030ea3
ChrisNieves <nievescs20@gmail.com>
2023-11-13 17:37:47 -0500

    forgot phoneNumber in thunk dispatch, added

--- src/client/store/reducers/playerInfo.js
+++ src/client/store/reducers/playerInfo.js
@@ -23,6 +23,7 @@ export const getPlayerInfoThunk = (playerId) => async (dispatch) => {
         firstName: player.firstName,
         lastName: player.lastName,
         email: player.email,
+        phoneNumber: player.phoneNumber,
       })
     );
   } catch (error) {
